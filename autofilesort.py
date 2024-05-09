aaef96cba1d876826d0bc70aab0079184a4608ed
TaiHoag <138497775+TaiHoag@users.noreply.github.com>
2024-05-09 18:28:31 +0700

    Add files via upload

---
+++ autofilesort.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Path of the sorting folder
+sorting_folder = r"C:\Users\Lenovo\Downloads"
+os.chdir(sorting_folder)
+path = os.getcwd()
+
+file_list = os.listdir(path)
+
+for file_ in file_list:
+    try:
+        name, ext = os.path.splitext(file_)
+        ext = ext[1:]
+
+        if ext == '':
+            continue
+
+        dest_folder = os.path.join(path, ext)
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+
+        dest_file_path = os.path.join(dest_folder, file_)
+        if not os.path.exists(dest_file_path):
+            shutil.move(os.path.join(path, file_), dest_file_path)
+    
+    except PermissionError:
+        continue
